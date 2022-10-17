053ae0fca23fa3c988cde444d8c1b56c2eb7c913
maxnilz <xianchao.yu.io@gmail.com>
2022-10-17 13:50:42 +0800

    implement insert & remove
    Signed-off-by: maxnilz <xianchao.yu.io@gmail.com>

--- bplustree.go
+++ bplustree.go
@@ -1,6 +1,16 @@
-package bplustree
+package main
 
-import "sort"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strings"
+)
 
 // items stores items in a node.
 type items[T any] []T
@@ -35,6 +45,14 @@ func (s *items[T]) pop() (out T) {
 	(*s)[index] = zero
 	*s = (*s)[:index]
 	return
+}
+
+// front returns the fist item or nil if it is empty.
+func (s *items[T]) front() (_ T) {
+	if len(*s) == 0 {
+		return
+	}
+	return (*s)[0]
 }
 
 // truncate truncates this instance at index so that it contains only the
@@ -91,16 +109,19 @@ type node[kT, vT any] struct {
 	values items[vT]
 }
 
-func (n *node[kT, vT]) degree() int {
-	return (n.order + 1) / 2
-}
-
 func (n *node[kT, vT]) maxKeys() int {
-	return n.order - 1
+	if !n.isLeaf {
+		return n.order - 1
+	}
+	return n.order
 }
 
 func (n *node[kT, vT]) minKeys() int {
-	return n.degree() - 1
+	degree := int(math.Ceil(float64(n.order) / 2.0))
+	if !n.isLeaf {
+		return degree - 1
+	}
+	return degree
 }
 
 // split splits the given node at the given index. The current node shrinks,
@@ -121,12 +142,16 @@ func (n *node[kT, vT]) split(i int) (kT, *node[kT, vT]) {
 		newNode.children = append(newNode.children, n.children[i+1:]...)
 		n.children.truncate(i + 1)
 	}
+	newNode.order = n.order
 	newNode.parent = n.parent
 	newNode.isLeaf = n.isLeaf
 	if len(n.values) > 0 {
 		newNode.values = append(newNode.values, n.values[i:]...)
+		n.values.truncate(i)
 	}
-	n.next.prev = newNode
+	if n.next != nil {
+		n.next.prev = newNode
+	}
 	newNode.prev = n
 	newNode.next = n.next
 	n.next = newNode
@@ -134,9 +159,10 @@ func (n *node[kT, vT]) split(i int) (kT, *node[kT, vT]) {
 }
 
 // insert inserts a key-value pair into the subtree rooted at this node,
-// making sure no nodes in the subtree exceed order-1 keys. If an equivalent
-// key be found/replaced by insert, it will be returned.
-func (n *node[kT, vT]) insert(key kT, value vT, less LessFunc[kT]) bool {
+// making sure no nodes in the subtree exceed order-1 keys. it will replace the value
+// if the given key existed already and return false to indicate that no new key is
+// inserted, otherwise, return the true and newly created node if a split happens.
+func (n *node[kT, vT]) insert(key kT, value vT, less LessFunc[kT]) (*node[kT, vT], bool) {
 	if n.isLeaf {
 		return n.insertIntoLeaf(key, value, less)
 	}
@@ -144,22 +170,20 @@ func (n *node[kT, vT]) insert(key kT, value vT, less LessFunc[kT]) bool {
 	return n.children[i].insert(key, value, less)
 }
 
-func (n *node[kT, vT]) insertIntoLeaf(key kT, value vT, less LessFunc[kT]) (found bool) {
-	var index int
-	index, found = n.keys.find(key, less)
+func (n *node[kT, vT]) insertIntoLeaf(key kT, value vT, less LessFunc[kT]) (*node[kT, vT], bool) {
+	index, found := n.keys.find(key, less)
 	if found {
 		n.values[index] = value
-		return
+		return nil, false
 	}
 	n.keys.insertAt(index, key)
 	n.values.insertAt(index, value)
-	n.mayGrowUp(less)
-	return
+	return n.mayGrowUp(less)
 }
 
-func (n *node[kT, vT]) mayGrowUp(less LessFunc[kT]) bool {
+func (n *node[kT, vT]) mayGrowUp(less LessFunc[kT]) (*node[kT, vT], bool) {
 	if len(n.keys) <= n.maxKeys() {
-		return false
+		return nil, false
 	}
 	promotedKey, newNode := n.split(n.minKeys())
 	parent := n.parent
@@ -171,21 +195,22 @@ func (n *node[kT, vT]) mayGrowUp(less LessFunc[kT]) bool {
 		root.children = append(root.children, n, newNode)
 		n.parent = root
 		newNode.parent = root
-		return true
+		return root, true
 	}
 
 	index, found := parent.keys.find(promotedKey, less)
 	if found {
 		panic("unexpected key found at parent")
 	}
-	promotedKey, newNode = parent.split(index)
 	parent.keys.insertAt(index, promotedKey)
-	parent.children.insertAt(index, newNode)
+	parent.children.insertAt(index+1, newNode)
 	return parent.mayGrowUp(less)
 }
 
 // remove removes an item from the subtree rooted at this node.
-func (n *node[kT, vT]) remove(key kT, less LessFunc[kT]) (_ vT, _ bool) {
+// if no key found in the leaf node of the subtree, return false, otherwise,
+// remove it from leaf node, return true and the stop node if merge happens.
+func (n *node[kT, vT]) remove(key kT, less LessFunc[kT]) (_ *node[kT, vT], _ vT, _ bool) {
 	if n.isLeaf {
 		return n.removeFromLeaf(key, less)
 	}
@@ -193,7 +218,7 @@ func (n *node[kT, vT]) remove(key kT, less LessFunc[kT]) (_ vT, _ bool) {
 	return n.children[i].remove(key, less)
 }
 
-func (n *node[kT, vT]) removeFromLeaf(key kT, less LessFunc[kT]) (out vT, found bool) {
+func (n *node[kT, vT]) removeFromLeaf(key kT, less LessFunc[kT]) (stopAt *node[kT, vT], out vT, found bool) {
 	var index int
 	index, found = n.keys.find(key, less)
 	if !found {
@@ -207,7 +232,7 @@ func (n *node[kT, vT]) removeFromLeaf(key kT, less LessFunc[kT]) (out vT, found 
 	if n.mayStealFromNeighborLeaf(key, less) {
 		return
 	}
-	n.mayMergeWithNeighbor(key, less)
+	stopAt, _ = n.mayMergeWithNeighbor(key, less)
 	return
 }
 
@@ -243,28 +268,152 @@ func (n *node[kT, vT]) mayStealFromNeighborLeaf(key kT, less LessFunc[kT]) bool 
 	return false
 }
 
-func (n *node[kT, vT]) mayMergeWithNeighbor(key kT, less LessFunc[kT]) bool {
-	parent := n.parent
+func (n *node[kT, vT]) mayMergeWithNeighbor(key kT, less LessFunc[kT]) (*node[kT, vT], bool) {
 	first, second := n.prev, n
 	if n.prev == nil {
 		first, second = n, n.next
 	}
 	if len(first.keys)+len(second.keys) > first.maxKeys() {
-		return false
+		return n, false
 	}
 
 	first.keys = append(first.keys, second.keys...)
 	first.children = append(first.children, second.children...)
-	first.values = append(first.values, first.values...)
+	first.values = append(first.values, second.values...)
+	first.next = second.next
+	if second.next != nil {
+		second.next.prev = first
+	}
 
-	i, _ := n.keys.find(key, less)
+	parent := n.parent
+	i, _ := parent.keys.find(key, less)
+	if i == len(parent.keys) {
+		i = i - 1
+	}
 	parent.keys.removeAt(i)
 	parent.children.removeAt(i + 1)
-	parent.mayMergeWithNeighbor(key, less)
+	if len(parent.keys) == 0 {
+		first.parent = nil
+		parent = nil
+		return first, true
+	}
+	stopAt, _ := parent.mayMergeWithNeighbor(key, less)
+	return stopAt, true
+}
 
-	return true
+func (n *node[kT, vT]) print(w io.Writer) error {
+	if n == nil {
+		return nil
+	}
+	q := items[*node[kT, vT]]{}
+	q = append(q, n)
+	out := &bytes.Buffer{}
+	for len(q) > 0 {
+		cnt := len(q)
+		for ; cnt > 0; cnt-- {
+			out.WriteString("| ")
+			a := q.removeAt(0)
+			for i, key := range a.keys {
+				if a.isLeaf {
+					out.WriteString(fmt.Sprintf("%v-%v ", key, a.values[i]))
+					continue
+				}
+				out.WriteString(fmt.Sprintf("%v ", key))
+			}
+			out.WriteString("|")
+			q = append(q, a.children...)
+		}
+		out.WriteString("\n")
+	}
+	if _, err := io.Copy(w, out); err != nil {
+		return err
+	}
+	return nil
 }
 
 // LessFunc determines how to order a type 'T'.  It should implement a strict
 // ordering, and should return true if within that ordering, 'a' < 'b'.
 type LessFunc[T any] func(a, b T) bool
+
+type BPlusTree[kT, vT any] struct {
+	order int
+	less  LessFunc[kT]
+	root  *node[kT, vT]
+}
+
+func New[kT, vT any](order int, less LessFunc[kT]) *BPlusTree[kT, vT] {
+	return &BPlusTree[kT, vT]{order: order, less: less}
+}
+
+func (t *BPlusTree[kT, vT]) Insert(key kT, value vT) bool {
+	if t.root == nil {
+		t.root = &node[kT, vT]{order: t.order, isLeaf: true}
+		t.root.keys = append(t.root.keys, key)
+		t.root.values = append(t.root.values, value)
+		return false
+	}
+	root, found := t.root.insert(key, value, t.less)
+	if root != nil {
+		t.root = root
+	}
+	return found
+}
+
+func (t *BPlusTree[kT, vT]) Remove(key kT) (_ vT, _ bool) {
+	if t.root == nil {
+		return
+	}
+	stopNode, out, found := t.root.remove(key, t.less)
+	if stopNode != nil && stopNode.parent == nil {
+		t.root = stopNode
+	}
+	return out, found
+}
+
+func (t *BPlusTree[kt, vT]) Print(w io.Writer) error {
+	if t.root == nil {
+		return nil
+	}
+	return t.root.print(w)
+}
+
+func main() {
+	order := 4
+	less := func(a, b int) bool { return a < b }
+	tree := New[int, int](order, less)
+	key, value := 0, 0
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		if len(text) == 0 {
+			continue
+		}
+		instruction := []byte(text)
+		cmd := instruction[0]
+		switch cmd {
+		case 'd':
+			_, err := fmt.Sscanf(string(instruction[1:]), "%d", &key)
+			if err != nil {
+				log.Panicln(err)
+			}
+			var ok bool
+			value, ok = tree.Remove(key)
+			if ok {
+				fmt.Printf("removed %d with value %d\n", key, value)
+			}
+			_ = tree.Print(os.Stdout)
+		case 'i':
+			_, err := fmt.Sscanf(string(instruction[1:]), "%d %d", &key, &value)
+			if err != nil {
+				log.Panicln(err)
+			}
+			tree.Insert(key, value)
+			out := bytes.Buffer{}
+			_ = tree.Print(&out)
+			fmt.Println(out.String())
+		}
+	}
+}
---
+++ bplustree_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	cases := []struct {
+		a      []int
+		search int
+		expect int
+	}{
+		{a: []int{1, 2, 3, 4}, search: 2, expect: 1},
+		{a: []int{1, 2, 4}, search: 3, expect: 2},
+		{a: []int{2}, search: 2, expect: 0},
+		{a: []int{2}, search: 3, expect: 0},
+		{a: []int{2}, search: 1, expect: 0},
+	}
+	less := func(a, b int) bool { return a < b }
+
+	for i, c := range cases {
+		n := strconv.Itoa(i)
+		t.Run(n, func(t *testing.T) {
+			input := items[int](c.a)
+			got, found := input.find(c.search, less)
+			fmt.Println(got, found, c.expect)
+		})
+	}
+}
